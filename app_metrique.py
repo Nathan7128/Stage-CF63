008c210d7490559362afea1a897cd54ae93681fe
Nathan7128 <164375966+Nathan7128@users.noreply.github.com>
2024-06-27 15:35:15 +0200

    maj de l'appli + fusions des docs skill corner et stats bomb

---
+++ app_metrique.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+col1, col2 = st.columns(2)
+
+with col1 :
+    annee = st.radio("Choisir année", options = ["2022_2023", "2023_2024"])
+
+with col2 :
+    choix_data = st.radio("Fournisseur data", options = ["Skill Corner", "Stats Bomb"])
+    if choix_data == "Skill Corner" :
+        path_student = ["student_physical.xlsx", "student_running.xlsx", "student_pressure.xlsx", "student_passe.xlsx"]
+        path_metrique = ["metrique_physical.xlsx", "metrique_running.xlsx", "metrique_pressure.xlsx", "metrique_passe.xlsx"]
+        liste_cat_met = ["Physiques", "Courses sans ballon avec la possession",
+                    "Action sous pression", "Passes à un coéquipier effectuant une course"]
+        cat_met = st.radio("Catégorie de métrique", liste_cat_met)
+        index_cat = liste_cat_met.index(cat_met)
+        file_student = path_student[index_cat]
+        file_metrique = path_metrique[index_cat]
+    else :
+        file_student = "student.xlsx"
+        file_metrique = "metrique.xlsx"
+    
+st.divider()
+
+student = pd.read_excel(f"Tableau métriques\\{annee}\\{choix_data}\\{file_student}")
+student.rename({student.columns[0] : "Métriques"}, axis = 1, inplace = True)
+pvalue = st.number_input("Valeur max pvalue", min_value=0.0, max_value=1.0, step = 0.001, format="%.3f")
+student_sort = student[student.pvalue < pvalue]
+st.write(f"Nombre de métriques gardées : {student_sort.shape[0]}")
+
+st.divider()
+
+st.markdown("<p style='text-align: center;'>Résultats des tests de student pour chaque métrique : plus la p-value est petite, plus la métrique différencie le top 5 des autres équipes de Ligue 2</p>", unsafe_allow_html=True)
+st.dataframe(student_sort, width = 500, hide_index=True)
+
+st.divider()
+
+metrique = pd.read_excel(f"Tableau métriques\\{annee}\\{choix_data}\\{file_metrique}", index_col=0)
+metrique_sort = metrique[student[student.pvalue < pvalue]["Métriques"].tolist()]
+st.markdown(f"<p style='text-align: center;'>Tableau des métriques (avec pvalue < {pvalue}) par équipes, en moyenne par match</p>", unsafe_allow_html=True)
+st.dataframe(metrique_sort)
