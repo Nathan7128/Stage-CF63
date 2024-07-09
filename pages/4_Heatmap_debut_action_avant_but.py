1f3466d01e9315e22157da743ce850ea70430854
Nathan7128 <164375966+Nathan7128@users.noreply.github.com>
2024-07-09 11:42:45 +0200

    V1 des heatmaps de débuts d'action avant un but terminer

--- pages/4_Heatmap_debut_action_avant_but.py
+++ pages/4_Heatmap_debut_action_avant_but.py
@@ -16,7 +16,7 @@ st.divider()
 
 pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0')
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4, gap = "large")
 
 with col1 :
     annee = st.radio("Choisir année", options = ["2021_2022", "2022_2023", "2023_2024"], horizontal = True)
@@ -28,22 +28,32 @@ with col3 :
     bins_v = st.number_input("Nombe de zones (vertical)", min_value = 1, step = 1, value = 5)
 
 with col4 :
-    top = st.multiselect("Groupe à afficher", options = ["Top 5", "Bottom 15", "Top 20"], default = ["Top 5", "Bottom 15", "Top 20"])
+    top = st.radio("Groupe à afficher", options = ["Top 5", "Bottom 15", "Top 20"], horizontal = True)
 
 st.divider()
 
 df = pd.read_excel(f"Heatmap SB/deb_action/Avant un but/Tableaux/{annee}/loc_deb_action.xlsx", index_col = 0)
 
-if "Top 5" in top :
-    col1, col2 = st.columns(2)
-    with col1 :
-        fig1, ax1 = pitch.draw()
-        fig1.set_facecolor('#f4edf0')
-        bin_statistic1 = pitch.bin_statistic(df[df["Top 5"]].x, df[df["Top 5"]].y, statistic='count', bins=(bins_h, bins_v), normalize=True)
-        pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#f9f9f9')
-        labels = pitch.label_heatmap(bin_statistic1, color='#f4edf0', ax = ax1, ha='center', va='center', str_format='{:.0%}')
-        st.pyplot(fig1)
-    with col2 :
-        fig2, ax2 = pitch.draw()
-        kde = pitch.kdeplot(df[df["Top 5"]].x, df[df["Top 5"]].y, cmap = cmr.nuclear, ax = ax2, fill = True, levels = 100, thresh = 0)
-        st.pyplot(fig2)
+if top == "Top 5" :
+    df_sort = df[df["Top 5"]]
+
+elif top == "Bottom 15" :
+    df_sort = df[~df["Top 5"]]
+
+else :
+    df_sort = df
+
+st.markdown(f"<p style='text-align: center;'>Heatmap pour le {top} de Ligue 2 sur la saison {annee}</p>", unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+with col1 :
+    fig1, ax1 = pitch.draw()
+    fig1.set_facecolor('#f4edf0')
+    bin_statistic1 = pitch.bin_statistic(df_sort.x, df_sort.y, statistic='count', bins=(bins_h, bins_v), normalize=True)
+    pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#f9f9f9')
+    labels = pitch.label_heatmap(bin_statistic1, color='#f4edf0', ax = ax1, ha='center', va='center', str_format='{:.0%}')
+    st.pyplot(fig1)
+with col2 :
+    fig2, ax2 = pitch.draw()
+    kde = pitch.kdeplot(df_sort.x, df_sort.y, cmap = cmr.nuclear, ax = ax2, fill = True, levels = 100, thresh = 0)
+    st.pyplot(fig2)
