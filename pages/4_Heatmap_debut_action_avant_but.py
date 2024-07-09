a6ef425caefd11e3cf2a39addd720d892cb067bf
Nathan7128 <164375966+Nathan7128@users.noreply.github.com>
2024-07-09 12:58:58 +0200

    modif appli heatmap deb action avant but

--- pages/4_Heatmap_debut_action_avant_but.py
+++ pages/4_Heatmap_debut_action_avant_but.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+import matplotlib.patheffects as path_effects
+
 import pandas as pd
 
 from mplsoccer import Pitch
@@ -14,18 +16,16 @@ st.title("Heatmap des zones de début d'actions menant à un but")
 
 st.divider()
 
-pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0')
-
 col1, col2, col3, col4 = st.columns(4, gap = "large")
 
 with col1 :
     annee = st.radio("Choisir année", options = ["2021_2022", "2022_2023", "2023_2024"], horizontal = True)
 
 with col2 :
-    bins_h = st.number_input("Nombe de zones (horizontal)", min_value = 1, step = 1, value = 6)
+    bins_h = st.number_input("Nombre de zone horizontale pour la Heatmap de gauche", min_value = 1, step = 1, value = 6)
 
 with col3 :
-    bins_v = st.number_input("Nombe de zones (vertical)", min_value = 1, step = 1, value = 5)
+    bins_v = st.number_input("Nombre de zone verticale pour la Heatmap de gauche)", min_value = 1, step = 1, value = 5)
 
 with col4 :
     top = st.radio("Groupe à afficher", options = ["Top 5", "Bottom 15", "Top 20"], horizontal = True)
@@ -45,15 +45,47 @@ else :
 
 st.markdown(f"<p style='text-align: center;'>Heatmap pour le {top} de Ligue 2 sur la saison {annee}</p>", unsafe_allow_html=True)
 
+
+@st.cache_data
+def heatmap_percen(data, bins_h, bins_v) :
+    path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
+    pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#C2BFBF")
+    fig1, ax1 = pitch.draw(constrained_layout=True, tight_layout=False)
+    fig1.set_facecolor("none")
+    ax1.set_facecolor("none")
+    fig1.set_edgecolor("none")
+    bin_statistic1 = pitch.bin_statistic(data.x, data.y, statistic='count', bins=(bins_h, bins_v), normalize=True)
+    pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#f9f9f9')
+    labels = pitch.label_heatmap(bin_statistic1, color='#f4edf0', ax = ax1, ha='center', va='center', str_format='{:.0%}', path_effects=path_eff)
+    st.pyplot(fig1)
+
+@st.cache_data
+def heatmap_smooth(data) :
+    path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
+    pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#C2BFBF")
+    fig2, ax2 = pitch.draw(constrained_layout=True, tight_layout=False)
+    fig2.set_facecolor("none")
+    ax2.set_facecolor("none")
+    fig2.set_edgecolor("none")
+    kde = pitch.kdeplot(data.x, data.y, ax = ax2, fill = True, levels = 100, thresh = 0, cmap = cmr.nuclear)
+    st.pyplot(fig2)
+
 col1, col2 = st.columns(2)
 with col1 :
-    fig1, ax1 = pitch.draw()
-    fig1.set_facecolor('#f4edf0')
-    bin_statistic1 = pitch.bin_statistic(df_sort.x, df_sort.y, statistic='count', bins=(bins_h, bins_v), normalize=True)
-    pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#f9f9f9')
-    labels = pitch.label_heatmap(bin_statistic1, color='#f4edf0', ax = ax1, ha='center', va='center', str_format='{:.0%}')
-    st.pyplot(fig1)
+    heatmap_percen(df_sort, bins_h, bins_v)
 with col2 :
-    fig2, ax2 = pitch.draw()
-    kde = pitch.kdeplot(df_sort.x, df_sort.y, cmap = cmr.nuclear, ax = ax2, fill = True, levels = 100, thresh = 0)
-    st.pyplot(fig2)
+    heatmap_smooth(df_sort)
+
+# @st.cache_data
+# def heatmap_func(data, bins_h, bins_v) :
+#     fig, ax = plt.subplots(nrows = 1, ncols = 2, facecolor = 'none', edgecolor = 'none')
+#     pitch = Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0')
+#     pitch.draw(ax = ax[0])
+#     bin_statistic1 = pitch.bin_statistic(data.x, data.y, statistic='count', bins=(bins_h, bins_v), normalize=True)
+#     pitch.heatmap(bin_statistic1, ax = ax[0], cmap = cmr.nuclear, edgecolor='#f9f9f9')
+#     pitch.label_heatmap(bin_statistic1, color='#f4edf0', ax = ax[0], ha='center', va='center', str_format='{:.0%}')
+#     pitch.draw(ax = ax[1])
+#     pitch.kdeplot(data.x, data.y, cmap = cmr.nuclear, ax = ax[1], fill = True, levels = 100, thresh = 0)
+#     st.pyplot(fig)
+
+# heatmap_func(df_sort, bins_h, bins_v)
