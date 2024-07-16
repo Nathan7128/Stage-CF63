9cc138427ce3843fb5a3eb355d2e8276b5ee3b09
Nathan7128 <164375966+Nathan7128@users.noreply.github.com>
2024-07-16 17:54:36 +0200

    modif graphe

--- Métriques discriminantes/Skill Corner/Évolution métriques/Par journée/physical.py
+++ Métriques discriminantes/Skill Corner/Évolution métriques/Par journée/physical.py
@@ -1,28 +1,22 @@
 import pandas as pd
 
 liste_dico = [{"match_drop" : [1550555, 1546206],
-                "ranking" : ["AJ Auxerre", "Angers SCO", "AS Saint-Étienne", "Rodez Aveyron", "Paris FC", "SM Caen", "Stade Lavallois Mayenne FC",
-           "Amiens Sporting Club", "En Avant de Guingamp", "Pau FC", "Grenoble Foot 38", "Girondins de Bordeaux", "SC Bastia",
-           "FC Annecy", "AC Ajaccio", "Dunkerque", "ES Troyes AC", "US Quevilly-Rouen", "US Concarneau", "Valenciennes FC"],
+                "ranking" : ["AJ Auxerre", "Angers SCO", "AS Saint-Étienne", "Rodez Aveyron", "Paris FC"],
            "annee" : "2023_2024"},
            {"match_drop" : [],
-                "ranking" : ["Le Havre AC", "FC Metz", "Girondins de Bordeaux", "SC Bastia", "SM Caen", "En Avant de Guingamp", "Paris FC",
-           "AS Saint-Étienne", "FC Sochaux-Montbéliard", "Grenoble Foot 38", "US Quevilly-Rouen", "Amiens Sporting Club", "Pau FC",
-           "Rodez Aveyron", "Stade Lavallois Mayenne FC", "Valenciennes FC", "FC Annecy", "Dijon FCO", "Nîmes Olympique", "Chamois Niortais FC"],
+                "ranking" : ["Le Havre AC", "FC Metz", "Girondins de Bordeaux", "SC Bastia", "SM Caen"],
            "annee" : "2022_2023"},
            {"match_drop" : [129364, 128946],
-                "ranking" : ["Toulouse FC", "AC Ajaccio", "AJ Auxerre", "Paris FC", "FC Sochaux-Montbéliard", "En Avant de Guingamp",
-                             "SM Caen", "Le Havre AC", "Nîmes Olympique", "Pau FC", "Dijon FCO", "SC Bastia", "Chamois Niortais FC", 
-                             "Amiens Sporting Club", "Grenoble Foot 38", "Valenciennes FC", "Rodez Aveyron", "US Quevilly-Rouen",
-                             "Dunkerque", "AS Nancy-Lorraine"],
+                "ranking" : ["Toulouse FC", "AC Ajaccio", "AJ Auxerre", "Paris FC", "FC Sochaux-Montbéliard"],
            "annee" : "2021_2022"}
            ]
+
 for dico in liste_dico :
      data_import = pd.read_excel(f"Data_file/Métriques Team sur une Saison Ligue 2 SB + SK/{dico["annee"]}/Skill Corner/data_physical.xlsx", index_col = 0)
      data = data_import[~(data_import.match_id.isin(dico["match_drop"]))]
      data.fillna(0, inplace = True)
 
-     drop = ["minutes_full_all", "player_name", "player_short_name", "player_id", "player_birthdate", "team_id", "match_name", "match_date", "competition_name", "competition_id", "season_name",
+     drop = ["player_name", "player_short_name", "player_id", "player_birthdate", "team_id", "match_name", "match_date", "competition_name", "competition_id", "season_name",
                "season_id", "competition_edition_id", "position", "position_group", "minutes_full_tip", "minutes_full_otip", "physical_check_passed"]
 
      data.drop(drop, inplace = True, axis = 1)
@@ -38,6 +32,15 @@ for dico in liste_dico :
      nb_joueur_match = data.apply(len)
      data = data.sum()
      data = data.divide(nb_joueur_match, axis = 0)
-     moyenne_journée = data.groupby("Journée").mean()
+
+     nb_minute_match = data.pop("minutes_full_all")
+     data = data.multiply(900/nb_minute_match, axis = 0)
+
+     moyenne_top20 = data.groupby("Journée").mean()
+     moyenne_top5 = data[data.index.get_level_values("team_name").isin(dico["ranking"])].groupby("Journée").mean()
+     moyenne_bottom15 = data[~data.index.get_level_values("team_name").isin(dico["ranking"])].groupby("Journée").mean()
+
      data.to_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{dico["annee"]}/Skill Corner/physical_équipe.xlsx")
-     moyenne_journée.to_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{dico["annee"]}/Skill Corner/physical_journée.xlsx")
+     moyenne_top20.to_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{dico["annee"]}/Skill Corner/physical_top20.xlsx")
+     moyenne_top5.to_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{dico["annee"]}/Skill Corner/physical_top5.xlsx")
+     moyenne_bottom15.to_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{dico["annee"]}/Skill Corner/physical_bottom15.xlsx")
--- pages/2_Évolutions_par_journée.py
+++ pages/2_Évolutions_par_journée.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
 st.title("Évolutions des métriques au cours des saisons")
 
-
+idx = pd.IndexSlice
 
 col1, col2 = st.columns(2, gap = "Large")
 
 with col1 :
-    choix_data = st.radio("Fournisseur data", options = ["Skill Corner", "Stats Bomb"], horizontal = True)
+    choix_data = st.radio("Fournisseur data", options = ["Skill Corner"], horizontal = True)
 
 with col2 :
     annee = st.radio("Choisir saison", options = ["2021_2022", "2022_2023", "2023_2024"], horizontal = True)
@@ -37,96 +38,70 @@ dico_type = {
 }
 liste_cat_type = st.multiselect("Type de la catégorie", dico_type[cat_met], default = dico_type[cat_met])
 
-st.divider()
+if len(liste_cat_type) > 0 :
+
+    st.divider()
 
 
-evo_équipe = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}équipe.xlsx", index_col = [0, 1])
-evo_journée = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}journée.xlsx", index_col = 0)
+    evo_équipe = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}équipe.xlsx", index_col = [0, 1])
 
-col_keep = [False]*evo_équipe.shape[1]
-if cat_met == "Physiques" :
-    for cat_type in liste_cat_type :
-        col_keep = np.logical_or(col_keep, ["_" + cat_type in i for i in evo_équipe.columns])
+    col_keep = [False]*evo_équipe.shape[1]
+    if cat_met == "Physiques" :
+        for cat_type in liste_cat_type :
+            col_keep = np.logical_or(col_keep, ["_" + cat_type in i for i in evo_équipe.columns])
 
-else :
-    for cat_type in liste_cat_type :
-        col_keep = np.logical_or(col_keep, [cat_type in i for i in evo_équipe.columns])
+    else :
+        for cat_type in liste_cat_type :
+            col_keep = np.logical_or(col_keep, [cat_type in i for i in evo_équipe.columns])
 
-evo_équipe = evo_équipe[evo_équipe.columns[col_keep]]
-evo_journée = evo_journée[evo_journée.columns[col_keep]]
+    evo_équipe = evo_équipe[evo_équipe.columns[col_keep]]
 
-choix_métriques = st.multiselect("Métriques gardées", options = evo_journée.columns)
-evo_équipe = evo_équipe[choix_métriques].reset_index().set_index("Journée")
-evo_journée = evo_journée[choix_métriques]
+    columns = st.columns(2)
+    with columns[0] :
+        met_graphe = st.selectbox("Métrique à afficher sur le graphe", evo_équipe.columns)
 
-if st.checkbox("Afficher le tableau des métriques par journée") :
-    st.dataframe(evo_journée, on_select = "rerun", selection_mode = "multi-column")
+    with columns[1] :
+        choix_groupe = st.selectbox("Groupe à afficher sur le graphe", options = ["Moyenne Top 5", "Moyenne Bottom 15", "Moyenne Top 5 & Bottom 15", "Moyenne globale", "Choisir équipe"])
 
-st.divider()
+    st.divider()
 
-columns = st.columns(2)
-with columns[0] :
-    if len(choix_métriques) > 0 :
-        met_graphe = st.radio("Métrique à afficher sur le graphe", choix_métriques, horizontal = True)
+    liste_graphe = []
 
-with columns[1] :
-    équipe_graphe = st.multiselect("Équipe à afficher sur le graphe (Sélectionner toutes les équipes pour avoir la moyenne du TOP 20)",
-                                   evo_équipe.team_name.unique(), default = evo_équipe.team_name.unique().tolist())
+    if choix_groupe == "Choisir équipe" :
+        équipe_graphe = st.multiselect("Équipe à afficher sur le graphe", evo_équipe.index.levels[1])
+        evo_équipe = evo_équipe.loc[idx[:, équipe_graphe], met_graphe]
+        for team in équipe_graphe :
+            liste_graphe.append(evo_équipe.loc[:, team, :])
+        title_graphe = f"Graphe des équipes sélectionnées pour la métrique {met_graphe}\nau cours des journées de la saison {annee}"
+        legend_graphe = équipe_graphe
 
+    elif choix_groupe == "Moyenne Top 5 & Bottom 15" :
+        top5_import = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}top5.xlsx", index_col = 0)
+        bottom15_import = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}bottom15.xlsx", index_col = 0)
 
-if len(équipe_graphe) == len(evo_équipe.team_name.unique().tolist()) :
-    st.line_chart(evo_journée[met_graphe])
-else :
-    st.dataframe(evo_équipe[evo_équipe.team_name.isin(équipe_graphe)].reset_index().set_index(["Journée", "team_name"])[met_graphe].unstack(level = "team_name"))
+        top5 = top5_import[met_graphe]
+        bottom15 = bottom15_import[met_graphe]
+        liste_graphe = [top5, bottom15]
+        legend_graphe = ["top5", "bottom15"]
+        title_graphe = f"Graphe du Top 5 et Bottom 15 pour la métrique {met_graphe}\nau cours des journées de la saison {annee}"
 
-# def couleur_text_df(col) :
-#     color = []
-#     for met in evo.index :
-#         if col.name == "Évolution en %" :
-#             if evo.loc[met, "2023_2024"] >= evo.loc[met, "2022_2023"] and evo.loc[met, "2022_2023"] >= evo.loc[met, "2021_2022"] :
-#                 color.append("background-color: rgba(0, 255, 0, 0.3)")
-#             elif evo.loc[met, "2023_2024"] >= evo.loc[met, "2022_2023"] and evo.loc[met, "2022_2023"] < evo.loc[met, "2021_2022"] :
-#                 color.append("background-color: rgba(0, 0, 255, 0.3)")
-#             elif evo.loc[met, "2023_2024"] < evo.loc[met, "2022_2023"] and evo.loc[met, "2022_2023"] >= evo.loc[met, "2021_2022"] :
-#                 color.append("background-color: rgba(255, 255, 0, 0.3)")
-#             else :
-#                 color.append("background-color: rgba(255, 0, 0, 0.3)")
-#         else :
-#             color.append('')
-#     return color
+    else :
+        dico_groupe = {"Moyenne Top 5" : "top5", "Moyenne Bottom 15" : "bottom15", "Moyenne globale" : "top20"}
+        evo_journée = pd.read_excel(f"Métriques discriminantes/Tableau métriques/Evolutions métriques/Par journée/{annee}/Skill Corner/{file_evo}{dico_groupe[choix_groupe]}.xlsx", index_col = 0)
+        evo_journée = evo_journée[met_graphe]
+        liste_graphe.append(evo_journée)
+        legend_graphe = ""
+        title_graphe = f"Graphe du {choix_groupe} de Ligue 2 pour la métrique {met_graphe}\nau cours des journées de la saison {annee}"
 
-# evo_style = evo.style.apply(couleur_text_df, axis = 0)
-
-# st.divider()
-
-# st.markdown("<p style='text-align: center;'>Tableau de l'évolution de chaque métrique entre la saison 2021/2022 et 2023/2024</p>", unsafe_allow_html=True)
-# met_sel = st.dataframe(evo_style, width = 10000, on_select = "rerun", selection_mode = "multi-row")
-
-# with st.columns([1.5, 4, 1])[1] :
-#     st.markdown("<p style='text-align: center;'>Code couleur de l'évolution des métriques entre la saison 2021/2022 et 2023/2024 :</p>", unsafe_allow_html=True)
-
-# col1, col2, col3, col4 = st.columns(4)
-# with col1 :
-#     "Vert : Strictement croissant"
-# with col2 :
-#     "Bleu : Décroissant puis croissant"
-# with col3 :
-#     "Jaune : Croissant puis décroissant"
-# with col4 :
-#     "Rouge : Strictement décroissant"
-
-# st.divider()
-
-
-# evo_graphe = evo_style.data.iloc[met_sel.selection.rows].drop("Évolution en %", axis = 1)
-# new_index = []
-# for i in evo_style.index[met_sel.selection.rows] :
-#     new_index.append(i[1] + " - " + i[0])
-# evo_graphe = evo_graphe.reset_index()
-# evo_graphe.index = new_index
-# # couleur = (evo_graphe.Top == "Top 5").replace({True : "#FF0000", False : '#0000FF'})
-# evo_graphe = evo_graphe.drop(["Métriques", "Top"], axis = 1)
-# contain1 = st.container(border = True)
-# with contain1 :
-#     st.markdown("<p style='text-align: center;'>Graphique de l'évolution des métriques sélectionnées</p>", unsafe_allow_html=True)
-#     st.line_chart(evo_graphe.T)
+    fig = plt.figure(figsize = (6, 3))
+    for graphe in liste_graphe :
+        plt.plot(graphe, linewidth = 0.7)
+    plt.title(title_graphe, fontweight = "heavy", y = 1.05, fontsize = 9)
+    plt.grid()
+    plt.legend(legend_graphe, bbox_to_anchor=(0.5, -0.25), fontsize = "small", ncol = 2)
+    plt.xlabel("Journée", fontsize = "small", fontstyle = "italic", labelpad = 10)
+    plt.ylabel("Métrique", fontsize = "small", fontstyle = "italic", labelpad = 10)
+    plt.tick_params(labelsize = 8)
+    ax = plt.gca()
+    ax.spines[:].set_visible(False)
+    st.pyplot(fig)
