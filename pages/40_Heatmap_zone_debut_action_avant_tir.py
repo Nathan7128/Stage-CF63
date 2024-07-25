cc875e79791042ad78bb10db4fc2b1bf972916ee
Nathan7128 <164375966+Nathan7128@users.noreply.github.com>
2024-07-25 11:06:21 +0200

    test

--- pages/40_Heatmap_zone_debut_action_avant_tir.py
+++ pages/40_Heatmap_zone_debut_action_avant_tir.py
@@ -12,8 +12,10 @@ import cmasher as cmr
 
 st.set_page_config(layout="wide")
 
-if "type_action" not in st.session_state :
-    st.session_state.type_action = []
+if "Type_action" not in st.session_state :
+    st.write("pas dedans")
+    st.session_state["Type_action"] = ["Open play"]
+
 
 st.title("Heatmap des zones de début d'actions menant à un tir")
 
@@ -131,82 +133,82 @@ else :
 
 #----------------------------------------------- FILTRAGE HEATMAPS ------------------------------------------------------------------------------------
 
+if len(df) > 0 :
+
+    columns = st.columns(2, gap = "large")
 
 
-columns = st.columns(2, gap = "large")
+    with columns[0] :
+        bins_h = st.number_input("Nombre de colonne pour la Heatmap de gauche", min_value = 1, step = 1, value = 5)
+        bins_v = st.number_input("Nombre de ligne pour la Heatmap de gauche", min_value = 1, step = 1, value = 10)
 
+    with columns[1] :
+        choix_goal = st.checkbox("Sélectionner uniquement les débuts d'actions menant à un but")
 
-with columns[0] :
-    bins_h = st.number_input("Nombre de colonne pour la Heatmap de gauche", min_value = 1, step = 1, value = 5)
-    bins_v = st.number_input("Nombre de ligne pour la Heatmap de gauche", min_value = 1, step = 1, value = 10)
+        if choix_goal :
+            df = df[df.But]
 
-with columns[1] :
-    choix_goal = st.checkbox("Sélectionner uniquement les débuts d'actions menant à un but")
-
-    if choix_goal :
-        df = df[df.But]
-
-    st.multiselect("Choisir le type de début d'action", options = df.type_action.unique(),
-                   default = st.session_state.type_action, key = "type_action")
-    df = df[df.type_action.isin(st.session_state.type_action)]
-
-st.divider()
-
-col1, col2 = st.columns(2, vertical_alignment = "bottom")
-with col1 :
-    choix_percent = st.checkbox("Cacher les '%' des zones")
-with col2 :
-    choix_line = st.checkbox("Cacher les lignes du terrain")
-
-
-
-#----------------------------------------------- AFFICHAGE HEATMAPS ------------------------------------------------------------------------------------
-
-if len(st.session_state.type_action) > 0 :
-    
+        type_action_choice = st.multiselect("Choisir le type de début d'action", options = df.type_action.unique(),
+                                            default = st.session_state["Type_action"], key = "Type_action")
+        # df = df[df.type_action.isin(st.session_state["Type action"])]
+    st.write(st.session_state["Type_action"])
     st.divider()
-
-    # if groupe != "Équipe" :
-    #     st.markdown(f"<p style='text-align: center;'>Heatmap pour le {groupe} de Ligue 2 sur la saison {annee_choice}</p>", unsafe_allow_html=True)
-
-    # else :
-    #     st.markdown(f"<p style='text-align: center;'>Heatmap pour les équipes sélectionnées de Ligue 2 sur la saison {annee_choice}</p>", unsafe_allow_html=True)
-
-
-
-    @st.cache_data
-    def heatmap_percen(data, bins_h, bins_v, choix_percent, choix_line) :
-        path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
-        pitch = VerticalPitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#f4edf0",
-                            goal_type = "box", linewidth = 1*(1 - choix_line), spot_scale = 0.002*(1 - choix_line))
-        fig1, ax1 = pitch.draw(constrained_layout=True, tight_layout=False)
-        fig1.set_facecolor("none")
-        ax1.set_facecolor((98/255, 98/255, 98/255))
-        fig1.set_edgecolor("none")
-        bin_statistic1 = pitch.bin_statistic(data.x, data.y, statistic='count', bins=(bins_v, bins_h), normalize=True)
-        pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#FF0000')
-        if not(choix_percent) :
-            labels = pitch.label_heatmap(bin_statistic1, fontsize = int(50/(bins_v + bins_h)) + 2, color='#f4edf0', ax = ax1,
-                                            ha='center', va='center', str_format='{:.0%}', path_effects=path_eff)
-        st.pyplot(fig1)
-
-    @st.cache_data
-    def heatmap_smooth(data, choix_line) :
-        path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
-        pitch = VerticalPitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#f4edf0",
-                            goal_type = "box", linewidth = 1*(1 - choix_line), spot_scale = 0.002*(1 - choix_line))
-        fig2, ax2 = pitch.draw(constrained_layout=True, tight_layout=False)
-        fig2.set_facecolor("none")
-        ax2.set_facecolor((98/255, 98/255, 98/255))
-        fig2.set_edgecolor("none")
-        kde = pitch.kdeplot(data.x, data.y, ax = ax2, fill = True, levels = 100, thresh = 0, cmap = cmr.nuclear)
-        st.pyplot(fig2)
-
-
-
 
     col1, col2 = st.columns(2, vertical_alignment = "bottom")
     with col1 :
-        heatmap_percen(df, bins_h, bins_v, choix_percent, choix_line)
+        choix_percent = st.checkbox("Cacher les '%' des zones")
     with col2 :
-        heatmap_smooth(df, choix_line)
+        choix_line = st.checkbox("Cacher les lignes du terrain")
+
+    df
+
+    #----------------------------------------------- AFFICHAGE HEATMAPS ------------------------------------------------------------------------------------
+
+    if len(st.session_state["Type action"]) > 0 :
+        
+        st.divider()
+
+        # if groupe != "Équipe" :
+        #     st.markdown(f"<p style='text-align: center;'>Heatmap pour le {groupe} de Ligue 2 sur la saison {annee_choice}</p>", unsafe_allow_html=True)
+
+        # else :
+        #     st.markdown(f"<p style='text-align: center;'>Heatmap pour les équipes sélectionnées de Ligue 2 sur la saison {annee_choice}</p>", unsafe_allow_html=True)
+
+
+
+        @st.cache_data
+        def heatmap_percen(data, bins_h, bins_v, choix_percent, choix_line) :
+            path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
+            pitch = VerticalPitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#f4edf0",
+                                goal_type = "box", linewidth = 1*(1 - choix_line), spot_scale = 0.002*(1 - choix_line))
+            fig1, ax1 = pitch.draw(constrained_layout=True, tight_layout=False)
+            fig1.set_facecolor("none")
+            ax1.set_facecolor((98/255, 98/255, 98/255))
+            fig1.set_edgecolor("none")
+            bin_statistic1 = pitch.bin_statistic(data.x, data.y, statistic='count', bins=(bins_v, bins_h), normalize=True)
+            pitch.heatmap(bin_statistic1, ax = ax1, cmap = cmr.nuclear, edgecolor='#FF0000')
+            if not(choix_percent) :
+                labels = pitch.label_heatmap(bin_statistic1, fontsize = int(50/(bins_v + bins_h)) + 2, color='#f4edf0', ax = ax1,
+                                                ha='center', va='center', str_format='{:.0%}', path_effects=path_eff)
+            st.pyplot(fig1)
+
+        @st.cache_data
+        def heatmap_smooth(data, choix_line) :
+            path_eff = [path_effects.Stroke(linewidth=1.5, foreground='black'), path_effects.Normal()]
+            pitch = VerticalPitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f4edf0', line_color = "#f4edf0",
+                                goal_type = "box", linewidth = 1*(1 - choix_line), spot_scale = 0.002*(1 - choix_line))
+            fig2, ax2 = pitch.draw(constrained_layout=True, tight_layout=False)
+            fig2.set_facecolor("none")
+            ax2.set_facecolor((98/255, 98/255, 98/255))
+            fig2.set_edgecolor("none")
+            kde = pitch.kdeplot(data.x, data.y, ax = ax2, fill = True, levels = 100, thresh = 0, cmap = cmr.nuclear)
+            st.pyplot(fig2)
+
+
+
+
+        col1, col2 = st.columns(2, vertical_alignment = "bottom")
+        with col1 :
+            heatmap_percen(df, bins_h, bins_v, choix_percent, choix_line)
+        with col2 :
+            heatmap_smooth(df, choix_line)
