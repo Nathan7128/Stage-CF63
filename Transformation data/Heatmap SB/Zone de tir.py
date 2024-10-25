67452bad9dcc3caae8aa8a4a9b9fe8d97e809b46
Nathan7128 <nathan.talbot28@gmail.com>
2024-10-25 11:59:52 +0200

    d

--- Transformation data/Heatmap SB/Zone de tir.py
+++ Transformation data/Heatmap SB/Zone de tir.py
@@ -3,19 +3,24 @@ import sqlite3
 
 connect = sqlite3.connect("raw-database.db")
 cursor = connect.cursor()
-
 colonnes = ["x_loc", "y_loc", "match_id", "type", "shot_type", "team", "minute", "player"]
 colonnes = ", ".join(colonnes)
+
 req = cursor.execute(f"SELECT {colonnes} FROM raw_data_heatmap")
+
 res = req.fetchall()
+
 desc = req.description
 df = pd.DataFrame(res)
 df.columns = [i[0] for i in desc]
 
 colonnes = ["Date", "Journée", "Domicile", "Extérieur", "Saison", "Compet", "match_id"]
 colonnes = ", ".join(colonnes)
+
 req = cursor.execute(f"SELECT {colonnes} FROM Info_matchs_SB")
+
 res = req.fetchall()
+
 desc = req.description
 df_infos = pd.DataFrame(res)
 df_infos.columns = [i[0] for i in desc]
